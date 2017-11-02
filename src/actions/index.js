c439539055e54d480ae3b071a18708071960a3fe
Sandi <sandi.c.gau@gmail.com>
2017-11-01 20:02:02 -0500

    fixed a few things

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+
+export const ADD_COMMENT = 'ADD_COMMENT'
+
+export function addComment () {
+  return {
+    type: ADD_COMMENT
+
+  }
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,21 @@
 import { combineReducers } from 'redux'
 
+import {
+  ADD_COMMENT
+} from '../actions'
+
+function post (state = {}, action) {
+  switch (action.type) {
+    case ADD_COMMENT :
+    const { comment } = action
+
+    return {
+      ...state,
+    }
+    default:
+    return state
+  }
+}
 
 export default combineReducers({
 
